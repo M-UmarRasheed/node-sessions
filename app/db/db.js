af4bf6ed91d67037427f315020914c0aa3c0e876
umar.rasheed-at-754199086007 <mumarrasheed69@gmail.com>
2022-11-21 17:10:12 +0500

    change db url to live

--- app/db/db.js
+++ app/db/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const router = express.Router()
-const url ="mongodb://localhost/session-crud"
+// const url ="mongodb://localhost/session-crud",
+const url = 'mongodb+srv://umar:umar123@cluster0.vaxmx8u.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(url,
     {
     useUnifiedTopology: true,
