b9f7fe2c6c0201a9400ab8c919bec5c866f633d6
umar.rasheed-at-754199086007 <mumarrasheed69@gmail.com>
2022-11-22 12:46:30 +0500

    remove db folder and connect with mongodb

--- app/app.js
+++ app/app.js
@@ -7,10 +7,40 @@ const sessionMiddleware = require('../app/middleware/sessionMiddleware')
 const apisMiddleware = require('./middleware/apisMiddleware')
 const aclMiddleware = require('./middleware/aclMiddleware')
 const bodyParser = require('body-parser')
+let mongoose = require('mongoose')
+// let cors = require('cors')
 
 let app = express()
-app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.json({ strict: false }))
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.get('/', (req, res) => res.json({ message: 'Welcome to our SSO Api !' }))
+
+let options = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	autoIndex: false, // Don't build indexes
+	serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+	socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+	family: 4, // Use IPv4, skip trying IPv6
+	keepAlive: 1,
+	connectTimeoutMS: 30000,
+	ssl: config.useSSL,
+	sslValidate: false,
+	// sslCA: fs.readFileSync('./rds-combined-ca-bundle.pem'),
+}
+
+// db connection
+mongoose.connect(config.DBHost, options)
+let db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+
+// var corsOptions = {
+// 	origin: true,
+// 	credentials: true,
+// 	methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+// 	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+// app.use(cors(corsOptions))
   var rolesContent = fs.readFileSync('config/settings/roles/roles.json')
   var roles = JSON.parse(rolesContent)
 
@@ -51,8 +81,6 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
   })
   app.use('/api', require('./routes/user.js').loginRouter)
   app.use('/api', require('./routes/session').loginRouter)
-
-  app.use('/api', require('../app/db/db').router)
 
   app.listen(
     config.PORT,
